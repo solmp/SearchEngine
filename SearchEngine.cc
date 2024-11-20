a225fa8e33b8d5fbbef8e24eaa77a5705f96e017
solmp <solmp163@163.com>
2024-11-20 10:24:53 +0800

    增加服务器main文件

---
+++ SearchEngine.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include "Configuration.h"
+#include "SearchEngineServer.h"
+
+int main(void) {
+  Configuration* config = Configuration::getInstance();
+  config->LoadConfig("../conf/config.json");
+  Dictionary* dict = Dictionary::getInstance();
+  dict->LoadDictionary(config);
+  json& server_conf = config->getConfigMap(SERVER);
+  string ip = server_conf["HOST"];
+  unsigned short port = server_conf["PORT"];
+  SearchEngineServer server(ip, port);
+  server.start();
+  return 0;
+}
